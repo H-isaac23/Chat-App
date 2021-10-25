a16c8c4b5a3b8250d9ce910c35a87f2422751513
H-isaac23 <dev.isaac23@gmail.com>
2021-10-25 22:28:19 +0800

    a lot of moving shit around

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const app = require('express')()
 const server = http.createServer(app)
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>')
