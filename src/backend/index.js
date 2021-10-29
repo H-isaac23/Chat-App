e2b475c8dae9fd660f34fca3c659b8a19d1105bd
H-isaac23 <dev.isaac23@gmail.com>
2021-10-29 23:25:08 +0800

    delete things

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,11 +2,20 @@ const http = require('http')
 const app = require('express')()
 const server = http.createServer(app)
 const cors = require('cors')
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
 server.listen('5000', () => {
--- src/frontend/src/services/getData.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:5000/'
-
-export const getData = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-}
