fe6464e002462eabbc7c7a22bb367690ac692b38
H-isaac23 <dev.isaac23@gmail.com>
2021-10-30 15:47:11 +0800

    basic chat app

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,23 +1,35 @@
 const http = require('http')
 const app = require('express')()
 const server = http.createServer(app)
-const cors = require('cors')
 const { Server } = require('socket.io')
 const io = new Server(server)
-
-app.use(cors())
+const PORT = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id)
+  console.log('a user connected', socket.id.substring(0, 3))
+
+  socket.broadcast.emit(
+    'user connect',
+    'a user connected ' + socket.id.substring(0, 3)
+  )
+
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    io.emit(
+      'user disconnect',
+      `a user with id ${socket.id.substring(0, 3)} disconnected`
+    )
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('Message: ', msg)
+    io.emit('chat message', msg)
   })
 })
 
-server.listen('5000', () => {
-  console.log('Listening on http://localhost:5000')
+server.listen(PORT, () => {
+  console.log('Server listening at http://localhost:3000')
 })
